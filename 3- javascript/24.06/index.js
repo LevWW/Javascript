26a8ce26fa8d0057052a8e62d1fdf1e08c8b6c90
LevWW <108127187+LevWW@users.noreply.github.com>
2022-06-24 13:48:56 +0300

    first lesson

---
+++ 3- javascript/24.06/index.js
@@ -0,0 +1,30 @@
+const products = [
+    {
+      name : "MokBook Thicc",
+      desc : "Greatest properly off ham exercise all.",
+      img : "dummy-pdt-b.jpg",
+      price : 2034
+    },
+    {
+      name : "MokBook Rookie",
+      desc : "Unsatiable its possession nor off.",
+      img : "dummy-pdt-b.jpg",
+      price : 1247
+    },
+   {
+      name : "iPong Max",
+      desc : "All difficulty unreserved the solicitude.",
+      img : "dummy-pdt-a.jpg",
+      price : 675
+    },
+     {
+      name : "iTab Pok",
+      desc : "Had judgment out property the supplied. ",
+      img : "dummy-pdt-a.jpg",
+      price : 842
+    }
+];
+
+//products[2]
+// document.getElementsByClassName('root').innerHTML = products[0].name
+
